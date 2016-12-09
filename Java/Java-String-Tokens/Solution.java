50f8c55c4786bb5abd339c3dbeb5abb81e5d9036
akashgoel92 <akashgoel92@gmail.com>
2016-12-09 12:14:19 -0500

    Update Solution.java

--- Java/Java-String-Tokens/Solution.java
+++ Java/Java-String-Tokens/Solution.java
@@ -1,15 +1,9 @@
     import java.io.*;
     import java.util.*;
 
-
-
     public class Solution {
-
-      
         public static void main(String[] args) 
         {
-
-          
           Scanner scan = new Scanner(System.in);
           String s=scan.nextLine();
         //Complete the code
